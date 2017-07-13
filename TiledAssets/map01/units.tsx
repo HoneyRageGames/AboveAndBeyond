5203c56406019640cbb71677ffa24fee9d73ef2f
Honey Rage Games <honeyragegames@gmail.com>
2017-07-12 23:40:21 -0700

    Add in new spacemap1 Tiled files

---
+++ TiledAssets/map01/units.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="shipAssets" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="../Assets/Resources/Textures/shipAssets.PNG" width="444" height="444"/>
+</tileset>
---
+++ TiledAssets/ships.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ships" tilewidth="69" tileheight="49" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="46" height="49" source="units/shipAssets_01.png"/>
+ </tile>
+ <tile id="1">
+  <image width="43" height="49" source="units/shipAssets_02.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="49" source="units/shipAssets_03.png"/>
+ </tile>
+ <tile id="3">
+  <image width="54" height="49" source="units/shipAssets_04.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="49" source="units/shipAssets_05.png"/>
+ </tile>
+ <tile id="5">
+  <image width="46" height="49" source="units/shipAssets_06.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="49" source="units/shipAssets_07.png"/>
+ </tile>
+ <tile id="7">
+  <image width="69" height="49" source="units/shipAssets_08.png"/>
+ </tile>
+ <tile id="8">
+  <image width="46" height="48" source="units/shipAssets_09.png"/>
+ </tile>
+ <tile id="9">
+  <image width="43" height="48" source="units/shipAssets_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="50" height="48" source="units/shipAssets_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="54" height="48" source="units/shipAssets_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="50" height="48" source="units/shipAssets_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="46" height="48" source="units/shipAssets_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="48" height="48" source="units/shipAssets_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="69" height="48" source="units/shipAssets_16.png"/>
+ </tile>
+</tileset>
